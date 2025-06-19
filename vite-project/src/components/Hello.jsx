ebf620e8a8ceee6bfc3427c84567eec1faa4e37a
waseemriyazz <waseemriyaz717@gmail.com>
2025-06-19 16:24:59 +0530

    jsx

--- vite-project/src/components/Hello.jsx
+++ vite-project/src/components/Hello.jsx
@@ -1,5 +1,17 @@
+const name = "Jack";
+function displayName(name) {
+  return `Hello, my name is ${name}`;
+}
+
 function Hello() {
-  return <h1>Hello from a component!</h1>;
+  return (
+    <div>
+      <h1>Hello from a component! </h1>
+      <h2>My name is {name}.</h2>
+      <h3>{displayName("Bob")}</h3>
+      <h4>Two and two are {2 + 2}</h4>
+    </div>
+  );
 }
 
 export default Hello;
