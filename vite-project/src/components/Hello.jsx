812d6802a1ffe42ee419fcbf0c2d619917ef6dfd
waseemriyazz <waseemriyaz717@gmail.com>
2025-06-19 19:19:10 +0530

    destructuring props

--- vite-project/src/components/Hello.jsx
+++ vite-project/src/components/Hello.jsx
@@ -3,7 +3,7 @@ function displayName(name) {
   return `Hello, my name is ${name}`;
 }
 
-function Hello(props) {
+function Hello({ name = "Default Name", message = "Default Message" }) {
   return (
     <div>
       <h1>Hello from a component! </h1>
@@ -11,8 +11,8 @@ function Hello(props) {
       <h3>{displayName("Bob")}</h3>
       <h4>Two and two are {2 + 2}</h4>
       <h1>****Data from props***</h1>
-      <h2>Hello prop name {props.name}.</h2>
-      <h2>Prop message {props.message}.</h2>
+      <h2>Hello prop name {name}.</h2>
+      <h2>Prop message {message}.</h2>
     </div>
   );
 }
