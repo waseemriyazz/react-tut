bc71103772b50bfc87e5388224df9b5544e1d9fd
waseemriyazz <waseemriyaz717@gmail.com>
2025-06-21 13:13:24 +0530

    rendeing an array of objects

--- vite-project/src/components/Fruits.jsx
+++ vite-project/src/components/Fruits.jsx
@@ -1,10 +1,19 @@
 export default function Fruits() {
-  const fruits = ["Apple", "Banana", "Cherry", "Date", "Elderberry"];
+  //   const fruits = ["Apple", "Banana", "Cherry", "Date", "Elderberry"];
+  const fruits = [
+    { key: 1, name: "Apple", emoji: "🍎", price: 12 },
+    { key: 2, name: "Banana", emoji: "🍌", price: 2 },
+    { key: 3, name: "Cherry", emoji: "🍒", price: 5 },
+    { key: 4, name: "Date", emoji: "🌴", price: 3 },
+    { key: 5, name: "Elderberry", emoji: "🍇", price: 8 },
+  ];
   return (
     <div>
       <ul>
         {fruits.map((fruit) => (
-          <li key={fruit}>{fruit}</li>
+          <li key={fruit.key}>
+            {fruit.emoji} {fruit.name} ${fruit.price}
+          </li>
         ))}
       </ul>
     </div>
