e73ae6417ee66be5e0bfff57f0a2128556086c36
waseemriyazz <waseemriyaz717@gmail.com>
2025-06-21 15:05:26 +0530

    conditionally rendring array elements

--- vite-project/src/components/Fruit.jsx
+++ vite-project/src/components/Fruit.jsx
@@ -1,7 +1,14 @@
 export default function Fruit({ fruit }) {
   return (
-    <li>
-      {fruit.emoji} {fruit.name} - ${fruit.price}
-    </li>
+    // <li>
+    //   {fruit.emoji} {fruit.name} - ${fruit.price}
+    // </li>
+    fruit.price > 3 ? (
+      <li>
+        {fruit.emoji} {fruit.name} - ${fruit.price}
+      </li>
+    ) : (
+      ""
+    )
   );
 }
