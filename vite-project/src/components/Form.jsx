c2f6c782bc2a09422ba2fcb9d75c46154495a736
waseemriyazz <waseemriyaz717@gmail.com>
2025-06-22 16:30:09 +0530

    handling multiple input feilds

--- vite-project/src/components/Form.jsx
+++ vite-project/src/components/Form.jsx
@@ -1,14 +1,22 @@
 import { useState } from "react";
 
 export default function Form() {
-  const [name, setName] = useState("");
+  const [name, setName] = useState({ firstname: "", lastname: "" });
   return (
     <div>
+      <h3>
+        {name.firstname} - {name.lastname}
+      </h3>
       <form>
         <input
-          onChange={(e) => setName(e.target.value)}
+          onChange={(e) => setName({ ...name, firstname: e.target.value })}
           type="text"
-          value={name}
+          value={name.firstname}
+        />
+        <input
+          onChange={(e) => setName({ ...name, lastname: e.target.value })}
+          type="text"
+          value={name.lastname}
         />
       </form>
     </div>
